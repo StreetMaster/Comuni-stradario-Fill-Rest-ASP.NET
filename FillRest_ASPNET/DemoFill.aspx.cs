3016ce0b2d8fd88996068abc16ea4b5f4106c1af
StreetMaster <supporto@streetmaster.it>
2016-10-21 10:17:52 +0200

    Aggiungere i file di progetto.

---
+++ FillRest_ASPNET/DemoFill.aspx.cs
@@ -0,0 +1,168 @@
+﻿using RestSharp;
+using System;
+
+namespace FillRest_ASPNET
+{
+    /// <summary>
+    /// Esempio di utilizzo del servizio WS FILL per la verifica e la normalizzazione degli indirizzi italiani 
+    /// 
+    /// L'end point del servizio è 
+    ///     http://ec2-46-137-97-173.eu-west-1.compute.amazonaws.com/smrest/fill
+    ///     
+    /// Per l'utilizzo registrarsi sul sito http://streetmaster.it e richiedere la chiave per il servizio FILL 
+    /// Il protocollo di comunicazione e' in formato JSON
+    /// Per le comunicazioni REST è utilizzata la libreria opensource RestSharp (http://restsharp.org/)
+    /// 
+    ///  2016 - Software by StreetMaster (c)
+    /// </summary>
+    /// 
+    public partial class DemoFill : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCallVerify_Click(object sender, EventArgs e)
+        {
+
+            outArea.Style["Border"] = "none";
+            outArea.Style["Border-color"] = "#336600";
+
+
+            // inizializzazione client del servizio FILL
+            var clientFill = new RestSharp.RestClient();
+            clientFill.BaseUrl = new Uri("http://ec2-46-137-97-173.eu-west-1.compute.amazonaws.com");
+
+            var request = new RestRequest("smrest/webresources/fill", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+
+            // valorizzazione input
+            // valorizzazione input
+            // per l'esempio viene valorizzato un insieme minimo dei parametri
+            request.AddParameter("Key", txtKey.Text);
+            request.AddParameter("Localita", txtComune.Text);
+            request.AddParameter("Cap", txtCap.Text);
+            request.AddParameter("Provincia", txtProv.Text);
+            request.AddParameter("Indirizzo", txtIndirizzo.Text);
+            request.AddParameter("Localita2", txtFrazione.Text);
+            request.AddParameter("Dug", String.Empty);
+            request.AddParameter("Civico", String.Empty);
+
+            // chiamata al servizio
+            var response = clientFill.Execute<FillResponse>(request);
+            var outCall = response.Data;
+
+
+            // output
+            if (outCall.Norm==1)
+            {
+                // verifica OK
+                txtCap.Text = outCall.Output[0].Cap;
+                txtProv.Text= outCall.Output[0].Prov;
+                txtComune.Text = outCall.Output[0].Comune;
+                txtFrazione.Text = outCall.Output[0].Frazione;
+                txtIndirizzo.Text = outCall.Output[0].Indirizzo;
+                string htmlOut = "<p><font color=\"green\">INDIRIZZO VALIDO</font></p>";
+
+                // e' riportato in output solo un sottoinsieme di esempio di tutti i valori restituiti
+
+                htmlOut += "<table>";
+
+                htmlOut += "<tr><td>Regione</td>";
+                htmlOut += "<td>" + outCall.Output[0].Detail.Regione +  "</td></tr>"; ;
+
+                htmlOut += "<tr><td>Istat Prov</td>";
+                htmlOut += "<td>" + outCall.Output[0].Detail.IstatProv + "</td></tr>"; ;
+
+                htmlOut += "<tr><td>Istat Comune</td>";
+                htmlOut += "<td>" + outCall.Output[0].Detail.IstatComune + "</td></tr>"; ;
+
+                htmlOut += "<tr><td>X</td>";
+                htmlOut += "<td>" + outCall.Output[0].Geocod.X + "</td></tr>"; ;
+
+                htmlOut += "<tr><td>Y</td>";
+                htmlOut += "<td>" + outCall.Output[0].Geocod.Y + "</td></tr>"; ;
+
+
+                htmlOut += "</table>";
+                outArea.InnerHtml = htmlOut;
+            }
+            else
+            {
+                // verifica KO, gestione errore
+
+                // errore di licenza
+                if (outCall.CodErr == 997)
+                    outArea.InnerHtml = "<p><font color=\"red\">LICENSE KEY NON RICONOSCIUTA</font></p>";
+                else if (outCall.CodErr == 123)
+                    outArea.InnerHtml = "<p><font color=\"red\">NON E' STATO VALORIZZATO IL COMUNE</font></p>";
+                else if (outCall.CodErr == 124)
+                    outArea.InnerHtml = "<p><font color=\"red\">COMUNE\\FRAZIONE NON RICONOSCIUTO</font></p>";
+                else if (outCall.CodErr == 125)
+                {
+                    String htmlOut= "<p><font color=\"red\">COMUNE\\FRAZIONE AMBIGUO</font></p>";
+
+                    htmlOut += "<table>";
+                    foreach (FillCand outElem in outCall.Output)
+                    {
+                        htmlOut += "<tr><td>";
+
+                        htmlOut += outElem.Cap + " "+ outElem.Comune+ " " + outElem.Prov;
+                        if (outElem.Frazione != string.Empty)
+                            htmlOut += " - " + outElem.Frazione;
+                        htmlOut += "</td></tr>";
+                    }
+                    htmlOut += "</table>";
+                    outArea.InnerHtml = htmlOut;
+                }
+                else if (outCall.CodErr == 466)
+                {
+                    txtCap.Text = outCall.Output[0].Cap;
+                    txtProv.Text = outCall.Output[0].Prov;
+                    txtComune.Text = outCall.Output[0].Comune;
+                    txtFrazione.Text = outCall.Output[0].Frazione;
+                    outArea.InnerHtml = "<p><font color=\"red\">NON E' STATO VALORIZZATO L'INDIRIZZO</font></p>";
+                }
+                else if (outCall.CodErr == 467)
+                {
+                    txtCap.Text = outCall.Output[0].Cap;
+                    txtProv.Text = outCall.Output[0].Prov;
+                    txtComune.Text = outCall.Output[0].Comune;
+                    txtFrazione.Text = outCall.Output[0].Frazione;
+                    outArea.InnerHtml = "<p><font color=\"red\">INDIRIZZO NON RICONOSCIUTO</font></p>";
+                }
+                else if (outCall.CodErr == 468)
+                {
+                    txtCap.Text = outCall.Output[0].Cap;
+                    txtProv.Text = outCall.Output[0].Prov;
+                    txtComune.Text = outCall.Output[0].Comune;
+                
+                    String htmlOut = "<p><font color=\"red\">INDIRIZZO AMBIGUO</font></p>";
+                    htmlOut += "<table>";
+                    foreach (FillCand outElem in outCall.Output)
+                    {
+                        htmlOut += "<tr><td>";
+
+                        htmlOut += outElem.Cap + " " + outElem.Indirizzo;
+                        if (outElem.Frazione != string.Empty)
+                            htmlOut += " (" + outElem.Frazione + ")";
+                        htmlOut += "</td></tr>";
+                    }
+                    htmlOut += "</table>";
+                    outArea.InnerHtml = htmlOut;
+                }
+                else if (outCall.CodErr == 455)
+                {
+                    txtCap.Text = outCall.Output[0].Cap;
+                    txtProv.Text = outCall.Output[0].Prov;
+                    txtComune.Text = outCall.Output[0].Comune;
+                    txtFrazione.Text = outCall.Output[0].Frazione;
+                    txtIndirizzo.Text = outCall.Output[0].Indirizzo;
+                    outArea.InnerHtml = "<p><font color=\"red\">CAP INCONGRUENTE SU VIA MULTICAP</font></p>";
+                }
+            }
+            outArea.Style["Border"] = "groove";
+        }
+    }
+}
---
+++ FillRest_ASPNET/FillResponse.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FillRest_ASPNET
+{
+  /// <summary>
+  /// Classe per deserializzazione della risposta JSON del servizio FILL
+  /// </summary>
+    class DetailElem
+    {
+        public string IstatRegione { get; set; }
+        public string Regione { get; set; }
+        public string IstatProv { get; set; }
+        public string IstatComune { get; set; }
+        public string ProvEstesa { get; set; }
+        public string CAB { get; set; }
+        public string Catasto { get; set; }
+        public string Belfiore { get; set; }
+        public int CodComune { get; set; }
+        public int CodStrada { get; set; }
+        public string Dug { get; set; }
+        public string Toponimo { get; set; }
+        public string Civico { get; set; }
+        public string ComuneMultiCap { get; set; }
+        public string StradaMultiCap { get; set; }
+    }
+
+    class ShortElem
+    {
+        public string Comune { get; set; }
+        public string Frazione { get; set; }
+        public string Dug { get; set; }
+        public string Toponimo { get; set; }
+        public string Indirizzo { get; set; }
+    }
+
+    class CivElem
+    {
+        public string CivAdd { get; set; }
+        public string CivShort { get; set; }
+        public int NumCivico { get; set; }
+        public string Esponente { get; set; }
+        public string CasellaPostale { get; set; }
+        public double KM { get; set; }
+        public string Colore { get; set; }
+        public string Presso { get; set; }
+    }
+
+    class AlterElem
+    {
+        public int Cap { get; set; }
+        public int Comune { get; set; }
+        public int Dug { get; set; }
+        public int Frazione { get; set; }
+        public int Prov { get; set; }
+        public int Via { get; set; }
+    }
+
+    class GeocodElem
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+        public string OMI { get; set; }
+        public string Sez1991 { get; set; }
+        public string Sez2001 { get; set; }
+        public string Sez2011 { get; set; }
+        public string Side { get; set; }
+        public string Warning { get; set; }
+    }
+
+    class PostalFormatElem
+    {
+        public string Comune { get; set; }
+        public string ComuneShort { get; set; }
+        public string Dug { get; set; }
+        public string DugCompl { get; set; }
+        public string Frazione { get; set; }
+        public string FrazioneShort { get; set; }
+        public string IsFrazionePT { get; set; }
+        public string IsStradaPT { get; set; }
+        public string RigaAggiuntiva { get; set; }
+        public string RigaEdificio { get; set; }
+        public string RigaIndirizzo { get; set; }
+        public string RigaLocalita { get; set; }
+        public string Toponimo { get; set; }
+        public string ToponimoShort { get; set; }
+    }
+
+    class FillCand
+    {
+        public string Prov { get; set; }
+        public string Comune { get; set; }
+        public string Frazione { get; set; }
+        public string Cap { get; set; }
+        public string Indirizzo { get; set; }
+        public int ScoreComune { get; set; }
+        public int ScoreStrada { get; set; }
+
+        public DetailElem Detail { get; set; }
+        public ShortElem Short { get; set; }
+        public CivElem Civico { get; set; }
+        public AlterElem Alter { get; set; }
+        public GeocodElem Geocod { get; set; }
+        public PostalFormatElem FormatoPostale { get; set; }
+    }
+
+    class FillResponse
+    {
+      
+        public int Norm { get; set; }
+        public int CodErr { get; set; }
+        public int NumCand { get; set; }
+        public List<FillCand> Output { get; set; }
+
+    }
+}
